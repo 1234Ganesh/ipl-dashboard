dda7262c12526a86ab519e33af3b0de0f44abeb8
1234Ganesh <1234Ganesh>
2023-08-15 13:03:03 +0000

    ipl-dashboard

---
+++ src/components/Home/index.js
@@ -0,0 +1,66 @@
+// Write your code here
+import './index.css'
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import {Component} from 'react'
+import TeamCard from '../TeamCard'
+
+class Home extends Component {
+  state = {
+    teamsData: [],
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    this.getTeamsList()
+  }
+
+  getTeamsList = async () => {
+    const response = await fetch('https://apis.ccbp.in/ipl')
+    const fetchData = await response.json()
+    const updatedData = fetchData.teams.map(eachData => ({
+      name: eachData.name,
+      imageUrl: eachData.team_image_url,
+      id: eachData.id,
+    }))
+    this.setState({teamsData: updatedData, isLoading: false})
+  }
+
+  renderTeamsList = () => {
+    const {teamsData} = this.state
+    return (
+      <ul className="team-list-item">
+        {teamsData.map(eachteam => (
+          <TeamCard key={eachteam.id} teamsData={eachteam} />
+        ))}
+      </ul>
+    )
+  }
+
+  renderLoader = () => (
+    <div data-testid="loader" className="loader-con">
+      <Loader type="Rings" color="00BFFF" height={80} width={80} />
+    </div>
+  )
+
+  render() {
+    const {isLoading} = this.state
+    return (
+      <div className="app-con">
+        <div className="ipl-con">
+          <div className="header-con">
+            <img
+              className="ipl-logo"
+              src="https://assets.ccbp.in/frontend/react-js/ipl-logo-img.png"
+              alt="ipl logo"
+            />
+            <h1 className="header-heading">IPL Dashboard</h1>
+          </div>
+          {isLoading ? this.renderLoader() : this.renderTeamsList()}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
